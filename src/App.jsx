46eb9ef2403f1d4d84d5b8798fdff83694c16273
Alitaqi <aalitaqi349@gmail.com>
2025-08-01 05:51:41 +0500

    Installed framer, lucide, sahdcn, router, axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-
+import { Button } from "@/components/ui/button"
+import { Search } from "lucide-react";
+import { motion } from "framer-motion";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className="bg-green-500 h-screen">
-  <h1 className="text-3xl italic underline text-red-500">wtf</h1>
-</div>
-
+    <Button>HEllo</Button>
+    <Search className="w-6 h-6 text-gray-700" />
+     <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 10 }}>
+      Hello Reportics
+    </motion.h1>
+      
     </>
   )
 }
